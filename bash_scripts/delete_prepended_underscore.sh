8909982d209a490271d860cee5f64dda82afea06
vindignia <vindigni.a@gmail.com>
2022-07-11 16:48:56 +0200

    playground plot indeces

---
+++ bash_scripts/delete_prepended_underscore.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd ..
+mv _01_download_data.Rmd 01_download_data.Rmd
+mv _02_integrate_igraph.Rmd 02_integrate_igraph.Rmd
+mv _03_connectance.Rmd 03_connectance.Rmd
+mv _04_integrate_bipartite.Rmd 04_integrate_bipartite.Rmd
+mv _05_reproducibility.Rmd 05_reproducibility.Rmd
---
+++ bash_scripts/prepend_underscore.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd ..
+mv 01_download_data.Rmd _01_download_data.Rmd
+mv 02_integrate_igraph.Rmd _02_integrate_igraph.Rmd
+mv 03_connectance.Rmd _03_connectance.Rmd
+mv 04_integrate_bipartite.Rmd _04_integrate_bipartite.Rmd
+mv 05_reproducibility.Rmd _05_reproducibility.Rmd
