868c476c84d5bf1613c1de3f4dcde71b33ca11df
vindignia <vindigni.a@gmail.com>
2022-09-22 16:32:28 +0200

    WIP nestedness pipeline

--- playground/nestedness_pipeline.R
+++ playground/nestedness_pipeline.R
@@ -36,23 +36,33 @@ class(NM) <- "numeric"
 sum(NM)
 values_fraction <- sum(NM)/(nrow(NM)*ncol(NM))
 
+nestedness_in <- weboflife::nestedness(NM)
+overlap_in <- compute_overlap(NM, NM)
 
 df <- NULL 
-df <- data.frame(0, 
-                 nestedness(NM),
-                 compute_overlap(NM, NM), 
-                 values_fraction)
-colnames(df) <- c("iter","nestedness","overlap", "v_fraction")
+df <- data.frame(0,nestedness_in,nestedness_in,nestedness_in,
+                 overlap_in,overlap_in,overlap_in,values_fraction)
 
-for (i in seq(1,2000,50)){
+colnames(df) <- c("iter","nest_equif","nest_cell","nest_swap",
+                  "overlap_equif","overlap_cell","overlap_swap",
+                  "v_fraction")
+
+
+for (i in seq(1,2001,50)){
   
   print(paste0("iter = ", i))
   
+  NM_equif <- weboflife::null_model(NM, i, model = "equifrequent")
+  NM_cell <- weboflife::null_model(NM, model = "cell")
   NM_swap <- weboflife::null_model(NM, i, model = "swap")
-
+  
   df <- rbind(df, data.frame(iter = i, 
-                             nestedness = nestedness(NM_swap),
-                             overlap = compute_overlap(NM, NM_swap), 
+                             nest_equif = weboflife::nestedness(NM_equif),
+                             nest_cell= weboflife::nestedness(NM_cell),
+                             nest_swap = weboflife::nestedness(NM_swap),
+                             overlap_equif = compute_overlap(NM, NM_equif), 
+                             overlap_cell = compute_overlap(NM, NM_cell), 
+                             overlap_swap = compute_overlap(NM, NM_swap), 
                              v_fraction = values_fraction)
   )
 }
@@ -60,22 +70,29 @@ for (i in seq(1,2000,50)){
 df %>% formattable()
 
 overlap_leg <- data.frame(
-  x = 250,
+  x = 750,
   y = 0.9,
   label = "overlap"
 )
 
 nestedness_leg <- data.frame(
-  x = 120,
-  y = 0.6,
+  x = 800,
+  y = 0.8,
   label = "nestedness"
 )
 
 
 ggplot() +
   ggtitle(paste0("perfectly nested M with ", round(values_fraction,3), " non-zero el. (swap null)")) +
-  geom_point(data = df, aes(iter, overlap), color = "blue", shape=1, size=1.5) +
-  geom_point(data = df, aes(iter, nestedness), color = "dark red", shape=1, size=1.5) +
+  geom_point(data = df, aes(iter, overlap_equif), color = "blue", shape=2, size=1.5) +
+  geom_point(data = df, aes(iter, nest_equif), color = "dark red", shape=2, size=1.5) +
+  #
+  geom_point(data = df, aes(iter, overlap_cell), color = "blue", shape=3, size=1.5) +
+  geom_point(data = df, aes(iter, nest_cell), color = "dark red", shape=3, size=1.5) +
+  #
+  geom_point(data = df, aes(iter, overlap_swap), color = "blue", shape=1, size=1.5) +
+  geom_point(data = df, aes(iter, nest_swap), color = "dark red", shape=1, size=1.5) +
+  #
   geom_line(data = df, aes(iter, 2*v_fraction-1), color = "black", size=0.6) +
   # geom_line(data = df, aes(iter, 1. - v_fraction), color = "grey", size=0.6) +
   geom_text(data=overlap_leg, aes( x=x, y=y, label=label),
@@ -85,6 +102,52 @@ ggplot() +
             color="dark red",
             size=4 , angle=0) + 
   labs(x = "iter", y = "overlap, nestedness") + ylim(0,1)
+
+
+# NULL models applied to nestedness
+
+nest_df <- NULL 
+
+for (ii in seq(1,100)){
+  
+  print(paste0("iter = ", ii))
+  
+  NM_equif <- weboflife::null_model(NM, model = "equifrequent")
+  NM_cell <- weboflife::null_model(NM, model = "cell")
+  NM_swap <- weboflife::null_model(NM, model = "swap")
+  
+  nest_df <- rbind(nest_df, data.frame(iter = ii, 
+                                       nest_equif = weboflife::nestedness(NM_equif),
+                                       nest_cell= weboflife::nestedness(NM_cell),
+                                       nest_swap = weboflife::nestedness(NM_swap)
+                                       ))
+}
+
+colnames(nest_df) <- c("iter","nest_equif","nest_cell","nest_swap")
+nest_df %>% formattable()
+
+# # Compute z-score for the nestedness values estimated by different null models 
+
+z_equif <- (nestedness_in - mean(nest_df$nest_equif))/sd(nest_df$nest_equif)
+z_cell <- (nestedness_in - mean(nest_df$nest_cell))/sd(nest_df$nest_cell)
+z_swap <- (nestedness_in - mean(nest_df$nest_swap))/sd(nest_df$nest_swap)
+
+
+#Fernando 
+
+#pollination_networks
+load("nullmodels_R/03-24_null_models_data/pollination_networks.Rdata")
+#seed_networks
+load("nullmodels_R/03-24_null_models_data/seed_networks.Rdata")
+
+# TODO: implement download of the same networks and convert in a suitable input for null models  
+# TODO: implement the loop above into a function which takes 
+#       the matrix, number of samplings and returns the z factor.   
+
+
+class(pollination_networks)
+pollination_networks$M_PL_008
+
 
 #VISUALIZATION:  
 # NM <- perfect_nested(n,m)
